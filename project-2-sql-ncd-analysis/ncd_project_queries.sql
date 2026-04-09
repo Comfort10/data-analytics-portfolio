bcf6cfdd6570ff4be7e270bd816a2106bf0da7dc
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-04-09 14:01:24 +0200

    Update ncd_project_queries.sql

--- project-2-sql-ncd-analysis/ncd_project_queries.sql
+++ project-2-sql-ncd-analysis/ncd_project_queries.sql
@@ -1,3 +1,27 @@
--- **Query 1: String Functions**
+-- Query 1: String Functions
+
+-- Standardising: Converting all province names to uppercase. 
+
+SELECT record_id, year, UPPER(province) AS province, diabetes_prevalence, hypertension_awareness,
+	hypertension_awareness, on_treatment, controlled, population_millions
+FROM ncd_data
+
+-- Province code: Extracting the first 3 characters of each province name as a short code column.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
