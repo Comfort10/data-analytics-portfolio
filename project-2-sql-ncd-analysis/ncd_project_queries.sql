5f4720fd80a372bd47a9f7e719b16787dd7bad5a
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-04-09 14:02:22 +0200

    Update ncd_project_queries.sql

--- project-2-sql-ncd-analysis/ncd_project_queries.sql
+++ project-2-sql-ncd-analysis/ncd_project_queries.sql
@@ -7,9 +7,10 @@ SELECT record_id, year, UPPER(province) AS province, diabetes_prevalence, hypert
 FROM ncd_data
 
 -- Province code: Extracting the first 3 characters of each province name as a short code column.
+SELECT record_id, province, UPPER(LEFT(province, 3)) AS province_code
+FROM ncd_data
 
-
-
+-- Filter by text: Return all records where the province name contains the word 'Cape'
 
 
 
