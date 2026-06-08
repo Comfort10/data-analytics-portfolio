619b392f9fb9ba3e72c4632c7b56e4b80046c3c5
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-06-08 20:25:13 +0200

    Update banking_queries.sql

--- project-5-sa-banking-analysis/banking_queries.sql
+++ project-5-sa-banking-analysis/banking_queries.sql
@@ -13,13 +13,13 @@ WHERE code = 'KBP1347M'
 
 /* 
 Query 2 — COVID period classification:
-"Using CASE, flag each month as 'Pre-COVID' (before March 2020), 'COVID Period' (March 2020 — December 2021),
-or 'Recovery' (2022 onwards)."
+Flag each month as 'Pre-COVID' (before March 2020), 'During-COVID' (March 2020 — December 2021),
+or 'Post-COVID' (2022 onwards).
 */
 SELECT date,
 	CASE
 		WHEN date < '2020-03-01' THEN 'Pre-COVID'
-		WHEN date BETWEEN '2020-03-01' and '2021-12-31' THEN 'COVID Period'
+		WHEN date BETWEEN '2020-03-01' and '2021-12-31' THEN 'During-COVID'
 		WHEN date >= '2022-01-01' THEN 'Post-COVID'
 	END AS COVID_period
 FROM sarb_monthly
