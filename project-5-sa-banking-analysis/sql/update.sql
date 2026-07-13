60cb736af4f9b963cdd45c00a395ee71d5d3b9c6
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-07-13 20:10:18 +0200

    Update codes in sarb_description table
    Update codes in sarb_description to match SARB_Banking_Data dataset.

--- project-5-sa-banking-analysis/sql/update.sql
+++ project-5-sa-banking-analysis/sql/update.sql
@@ -1,1 +1,14 @@
+-- Update the codes so that they are identical to the codes from the SARB_Banking_Data dataset
+
+UPDATE sarb_description
+SET code = CASE code
+	WHEN 'KBP1008' THEN 'KBP1008M'
+	WHEN 'KBP1123' THEN 'KBP1123M'
+	WHEN 'KBP1132' THEN 'KBP1132M'
+	WHEN 'KBP1166' THEN 'KBP1166M'
+	WHEN 'KBP1347' THEN 'KBP1347M'
+	WHEN 'KBP1486' THEN 'KBP1486M'
+END 
+
+WHERE code IN ('KBP1008', 'KBP1123', 'KBP1132', 'KBP1166', 'KBP1347', 'KBP1486') 
 
