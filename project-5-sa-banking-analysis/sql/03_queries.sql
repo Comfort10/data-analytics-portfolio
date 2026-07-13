089f20770f5260195eded0cbf46a856eb9385bc4
Phumza Nkosi <88560590+Comfort10@users.noreply.github.com>
2026-07-13 20:43:15 +0200

    Update 03_queries.sql

--- project-5-sa-banking-analysis/sql/03_queries.sql
+++ project-5-sa-banking-analysis/sql/03_queries.sql
@@ -7,8 +7,11 @@ Query 1 — Monthly trend:
 Show all monthly PSCE values. Add a column showing month-on-month change.
 */
 
-SELECT date, code, value,
-	LAG(value) OVER(PARTITION BY code
+SELECT 
+	date, 
+	code, 
+	value,
+	value - LAG(value) OVER(PARTITION BY code
 						          ORDER BY date) AS month_on_month					
 FROM sarb_monthly
 
